2c23c41a12da482038600037386d9fea99d7e040
Ignacio González <ignacio.gonzalez1901@alumnos.ubiobio.cl>
2022-10-16 01:09:58 -0300

    Renderizado condicionales para pagina activa y para mostrar navbar

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
-import { Heading, Stack, FormControl, Input, FormLabel, InputGroup, Button, InputRightElement, Container, Link, Tooltip, Flex } from '@chakra-ui/react';
+import { Heading, Stack, FormControl, Input, FormLabel, InputGroup, Button, InputRightElement, Container, Link, Tooltip, Flex, Image } from '@chakra-ui/react';
 import postLogin from '../data/postLogin';
 import Cookies from 'js-cookie'
 import { useRouter } from 'next/router'
+import Swal from 'sweetalert2';
+import image from '../public/background.jpg'
 
 const Home = () => {
 
@@ -23,38 +25,53 @@ const Home = () => {
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		console.log(user);
 		try {
 			postLogin(user.email, user.password)
 				.then((res) => {
 					Cookies.set("token", res.data.token, { expires: 1 })
 					Cookies.set("user", res.data.user, { expires: 1 })
+					Router.push('/servicios')
 				})
 				.catch((err) => {
-					console.log(err);
+					Swal.fire({
+						icon: 'error',
+						title: 'Oops...',
+						text: 'Usuario o contraseña incorrectos',
+					})
 				})
 		} catch (error) {
-			console.log(error)
+			Swal.fire({
+				icon: 'error',
+				title: 'Oops...',
+				text: 'Usuario o contraseña incorrectos',
+			})
 		}
 	}
 
+	const enterKeyHandler = event => {
+		if (event.key === 'Enter') {
+			event.preventDefault();
+			handleSubmit(event);
+		}
+	};
+
 	return (
-		<Flex w={'full'} h={"100vh"} justify={"center"} align="center">
-			<Container maxW="container.md" justifyContent={"center"} alignItems={"center"} >
-				<Stack spacing={4} justify={"center"}>
-					<Heading as="h1" size="xl" textAlign={"center"}>Plataforma de Cotizaciones Estudio Fragua</Heading>
-					<Heading as="h2" size="md" textAlign={"center"}>Para utilizar la plataforma, por favor inicie sesión</Heading>
+		<Flex w={'full'} h={"100vh"} justify={"center"} align="center" backgroundImage="url('/background.jpg')" bgPos={"center"} filter='auto' >
+			<Container maxW="container.md" justifyContent={"center"} alignItems={"center"} backdropFilter='auto' backdropContrast='85%' p={'14'} >
+				<Stack spacing={4} justify={"center"} >
+					<Heading as="h1" size="xl" textAlign={"center"} textShadow={'2px 2px black'}>Plataforma de Cotizaciones Estudio Fragua</Heading>
+					<Heading as="h2" size="md" textAlign={"center"} textShadow={'2px 2px black'}>Para utilizar la plataforma, por favor inicie sesión</Heading>
 					<FormControl id="email">
 						<FormLabel>Correo Electrónico</FormLabel>
 						<Tooltip label="Ingresar Correo Electrónico" aria-label="Correo Electrónico">
-							<Input placeholder="Ej: correo@mail.com" type="email" onChange={handleChange} name="email" />
+							<Input placeholder="Ej: correo@mail.com" color={"orange"} focusBorderColor={"yellow.600"} type="email" onChange={handleChange} name="email" />
 						</Tooltip>
 					</FormControl>
 					<FormControl id="password">
 						<FormLabel>Contraseña</FormLabel>
 						<Tooltip label="Ingresar contraseña" aria-label="A tooltip">
-							<InputGroup size='md'>
-								<Input pr='4.5rem' type={showInput ? 'password' : 'text'} placeholder='Contraseña' onChange={handleChange} name="password" />
+							<InputGroup size='md' onKeyDown={enterKeyHandler}>
+								<Input pr='4.5rem' type={showInput ? 'password' : 'text'} color={"orange"} focusBorderColor={"yellow.600"} placeholder='Contraseña' onChange={handleChange} name="password" />
 								<InputRightElement mr={"1"} width='4.5rem'>
 									<Button h='1.75rem' size='sm' colorScheme={"orange"} onClick={handleClick}>{showInput ? 'Mostrar' : 'Ocultar'}</Button>
 								</InputRightElement>
