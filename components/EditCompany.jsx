70a33ce3de302481288e69ef41f806611d879c5d
Ignacio González <ignacio.gonzalez1901@alumnos.ubiobio.cl>
2022-12-04 11:22:18 -0300

    Componente para stepper en editar empresa

--- components/EditCompany.jsx
+++ components/EditCompany.jsx
@@ -1,11 +1,8 @@
 import { useState } from 'react'
-import { useRouter } from 'next/router'
 import ContactFormEdit from './ContactFormEdit';
 import CompanyFormEdit from './CompanyFormEdit';
 
 const EditCompany = ({ step, setStep, company, setCompany }) => {
-    const router = useRouter()
-    const { empresa } = router.query
     const [companyRUT, setCompanyRUT] = useState(company.rut)
     const [contactRUT, setContactRUT] = useState(company.contact.rut)
 
@@ -14,7 +11,7 @@ const EditCompany = ({ step, setStep, company, setCompany }) => {
     }
     if (step === 2) {
         return (
-            <ContactFormEdit company={company} setStep={setStep} contact={company.contact} setContact={setCompany} contactRUT={contactRUT} setContactRUT={setContactRUT} />
+            <ContactFormEdit company={company} setStep={setStep} contact={company.contact} setContact={setCompany} contactRUT={contactRUT} setContactRUT={setContactRUT} state={company.state} companyRUT={companyRUT} />
         )
     }
 }
