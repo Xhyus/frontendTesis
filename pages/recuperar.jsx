4755170816b22af5ffd88521479d9d072dc2ab1a
Ignacio González <ignacio.gonzalez1901@alumnos.ubiobio.cl>
2022-12-08 00:54:46 -0300

    Generacion vista recuperar contraseña

---
+++ pages/recuperar.jsx
@@ -0,0 +1,67 @@
+import { useState } from 'react'
+import { Heading, Stack, FormControl, Input, FormLabel, InputGroup, Button, InputRightElement, Container, Link, Tooltip, Flex } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import { recoverPassword } from '../data/user';
+import Swal from 'sweetalert2';
+
+const recuperar = () => {
+    const [user, setUser] = useState({
+        email: ''
+    })
+    const Router = useRouter();
+    const handleChange = (e) => {
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await recoverPassword(user.email)
+            if (response.status === 200) {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Recuperación de contraseña',
+                    text: 'Se ha enviado un correo a su cuenta de correo electronico',
+                }).then(() => {
+                    Router.push("/")
+                })
+            }
+        } catch (error) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Ha ocurrido un error, el correo no existe, no esta asociado a una cuenta o no se ha podido enviar el correo',
+            })
+        }
+    }
+
+    const enterKeyHandler = event => {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+            handleSubmit(event);
+        }
+    };
+
+    return (
+        <Flex w={'full'} h={"100vh"} justify={"center"} align="center" backgroundImage="url('/background.jpg')" bgPos={"bottom"} filter='auto' >
+            <Container maxW="container.md" justifyContent={"center"} alignItems={"center"} backdropFilter='auto' backdropContrast='85%' p={'14'} >
+                <Stack spacing={4} justify={"center"} >
+                    <Heading as="h1" size="xl" textAlign={"center"} textShadow={'2px 2px black'}>Recuperar Contraseña</Heading>
+                    <Heading as="h2" size="md" textAlign={"center"} textShadow={'2px 2px black'}>Para solicitar una nueva contraseña escriba su correo electronico asociado a una cuenta de la plataforma</Heading>
+                    <FormControl id="email">
+                        <FormLabel>Correo Electrónico</FormLabel>
+                        <Tooltip label="Ingresar Correo Electrónico" aria-label="Correo Electrónico">
+                            <Input onKeyDown={enterKeyHandler} placeholder="Ej: correo@mail.com" color={"orange"} focusBorderColor={"yellow.600"} type="email" onChange={handleChange} name="email" />
+                        </Tooltip>
+                    </FormControl>
+                    <Button colorScheme="orange" size="md" w={'full'} onClick={handleSubmit}>Recuperar contraseña</Button>
+                    <Link onClick={() => Router.push("/")} color="orange.500" textAlign={"center"}>Volver al inicio</Link>
+                </Stack>
+            </Container>
+        </Flex>)
+}
+
+export default recuperar
