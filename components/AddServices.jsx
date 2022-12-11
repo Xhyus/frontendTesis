a144b40054b48b837e80999aedca02407838c285
Ignacio González <ignacio.gonzalez1901@alumnos.ubiobio.cl>
2022-12-11 18:33:30 -0300

    Se refactoriza vista para mejor manejo de informacion

--- components/AddServices.jsx
+++ components/AddServices.jsx
@@ -1,15 +1,53 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import SearchLook from './SearchLook'
 import { Container, Heading, Wrap, Stack, Text, Button, HStack } from '@chakra-ui/react'
 
-const AddServices = ({ services, selectedServices, cardList, setSearchTerm, searchTerm, setSearch, setStep, step, filter, filteredServices }) => {
+const AddServices = ({ services, selectedServices, cardList, setStep, step }) => {
+    const [filter, setFilter] = useState({
+        status: false,
+        filteredServices: [],
+        searchTerm: ''
+    })
+    useEffect(() => {
+        const results = services.filter(service => {
+            return (
+                service.name.toLowerCase().includes(filter.searchTerm.toLowerCase()) ||
+                service.description.toLowerCase().includes(filter.searchTerm.toLowerCase()) ||
+                service.price.toString().includes(filter.searchTerm) ||
+                service.type.toLowerCase().includes(filter.searchTerm.toLowerCase()) ||
+                service.item.some(item => item.description.toLowerCase().includes(filter.searchTerm.toLowerCase()))
+            )
+        })
+        setFilter({
+            ...filter,
+            filteredServices: results
+        })
+    }, [filter.searchTerm])
+
+    const setSearch = (e) => {
+        if (e.target.value.length > 0) {
+            setFilter({
+                ...filter,
+                status: true,
+                searchTerm: e.target.value
+            })
+        } else {
+            setFilter({
+                status: false,
+                searchTerm: '',
+                filteredServices: []
+            })
+        }
+    }
+
     return (
         <>
             <Container maxW={"container.xl"} centerContent>
                 <Heading mt={10} mb={5} fontSize={'6xl'}>Crear Cotización</Heading>
-                <SearchLook setSearchTerm={setSearchTerm} searchTerm={searchTerm} setSearch={setSearch} text={"Ver cotización"} setStep={setStep} step={step} />
+                <SearchLook searchTerm={filter.searchTerm} setSearch={setSearch} text={"Ver cotización"} setStep={setStep} step={step} filter={filter} setFilter={setFilter} />
                 <Wrap spacing={10} justify={{ base: "center", md: "normal" }} pb={20}>
-                    {cardList(filter ? filteredServices : services)}
+                    {cardList(filter.status === true ? filter.filteredServices : services)}
+                    {/* {cardList(filter.filteredServices)} */}
                 </Wrap>
             </Container >
             {selectedServices.length > 0 &&
