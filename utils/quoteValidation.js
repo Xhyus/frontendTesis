a8ff0f829e4b331c7c7c9fa715fd57751093f2cb
Ignacio González <ignacio.gonzalez1901@alumnos.ubiobio.cl>
2022-12-11 18:32:32 -0300

    Se arreglan las validaciones para formulario de creacion de cotizaciones

--- utils/quoteValidation.js
+++ utils/quoteValidation.js
@@ -11,20 +11,11 @@ const quoteValidation = yup.object({
         .min(3, 'La descripción debe tener al menos 3 caracteres')
         .max(100, 'La descripción debe tener máximo 100 caracteres')
         .matches(/^[a-zA-Z0-9áéíóúÁÉÍÓÚ\s\-\_\.\,\;\:\(\)\[\]\{\}\¿\?\¡\!\@\#\$\%\^\&\*\+\|\?\/\\]+$/, 'La descripción no puede contener caracteres especiales'),
-    formalization: yup.string()
-        .required('La formalización es obligatoria')
-        .oneOf(['Firma', 'Contrato', 'Confidencialidad'], 'La formalización debe ser Firma, Contrato o Confidencialidad'),
-    payment: yup.string()
-        .required('El pago es obligatorio')
-        .oneOf(['Efectivo', 'Credito', 'Transferencia', 'Debito', 'Otros'], 'El pago debe ser Efectivo, Credito, Transferencia, Debito u Otros'),
     paymentMethod: yup.string()
         .required('El método de pago es obligatorio')
         .min(3, 'El método de pago debe tener al menos 3 caracteres')
         .max(150, 'El método de pago debe tener máximo 150 caracteres')
-        .matches(/^[a-zA-Z0-9áéíóúÁÉÍÓÚ\s\-\_\.\,\;\:\(\)\[\]\{\}\¿\?\¡\!\@\#\$\%\^\&\*\+\|\?\/\\]+$/, 'El método de pago no puede contener caracteres especiales'),
-    documents: yup.string()
-        .required('El documento es obligatorio')
-        .oneOf(['Exenta', 'Afecta'], 'El documento debe ser Exenta o Afecta'),
+        .matches(/^[a-zA-Z0-9áéíóúÁÉÍÓÚ\s\-\_\.\,\;\:\(\)\[\]\{\}\¿\?\¡\!\@\#\$\%\^\&\*\+\|\?\/\\]+$/, 'El método de pago no puede contener caracteres especiales')
 })
 
 export default quoteValidation;
