104e51fac29c5f73cc0dd016a4026cd393356eb1
Ignacio González <ignacio.gonzalez1901@alumnos.ubiobio.cl>
2023-01-01 01:57:29 -0300

    Se agregan funcionalidades genericas

--- utils/formatInfo.js
+++ utils/formatInfo.js
@@ -2,6 +2,10 @@ const formatTitle = (title) => {
     return title.length > 15 ? title.charAt(0).toUpperCase() + title.slice(1, 15) + '...' : title.charAt(0).toUpperCase() + title.slice(1);
 }
 
+const formatTitleDetail = (title) => {
+    return title.length > 30 ? title.charAt(0).toUpperCase() + title.slice(1, 30) + '...' : title.charAt(0).toUpperCase() + title.slice(1);
+}
+
 const formatDescription = (description) => {
     return description.charAt(0).toUpperCase() + description.slice(1) + (description.length > 200 ? '...' : '');
 }
@@ -22,10 +26,29 @@ const formatDate = (date) => {
     return newDate.toLocaleString();
 }
 
+const formatText = (text) => {
+    return text.charAt(0).toUpperCase() + text.slice(1);
+}
+
+const formatFormalization = (formalization) => {
+    if (formalization === 'Firma') {
+        return 'Firma del presente contrato'
+    }
+    if (formalization === 'Contrato') {
+        return 'Firma de contrato de servicio'
+    }
+    if (formalization === 'Confidencialidad') {
+        return 'Firma de contrato de servicio y confidencialidad'
+    }
+}
+
 module.exports = {
     formatTitle,
     formatDescription,
     formatPrice,
     formatType,
     formatDate,
+    formatTitleDetail,
+    formatText,
+    formatFormalization
 }
