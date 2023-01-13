7d6c048030cb3d591fae6b9646d38b59bcaf79cc
Ignacio González <ignacio.gonzalez1901@alumnos.ubiobio.cl>
2023-01-12 22:29:23 -0300

    Componente para presentar informacion

---
+++ components/TagText.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { HStack, Text } from '@chakra-ui/react'
+
+const TagText = ({ tag, data }) => {
+    return (
+        <HStack align={"start"}>
+            <Text fontWeight={'bold'}>{tag}: </Text>
+            <Text>{data.length === 0 ? 'Aún no se han ingresado datos.' : data}</Text>
+        </HStack>
+    )
+}
+
+export default TagText
