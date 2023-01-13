d469234066961a2b7cf9a05f3ad1b2389d8ab5e2
Ignacio González <ignacio.gonzalez1901@alumnos.ubiobio.cl>
2023-01-13 01:28:38 -0300

    Refactorizacion del listado de servicios

--- components/AddServices.jsx
+++ components/AddServices.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react'
 import SearchLook from './SearchLook'
-import { Container, Heading, Wrap, Stack, Text, Button, HStack } from '@chakra-ui/react'
+import { Container, Heading, Wrap, Stack, Text, Button, HStack, WrapItem } from '@chakra-ui/react'
+import ServiceQuote from './ServiceQuote'
 
-const AddServices = ({ services, selectedServices, cardList, setStep, step }) => {
+const AddServices = ({ services, selectedServices, setStep, step, setSelectedServices }) => {
     const [filter, setFilter] = useState({
         status: false,
         filteredServices: [],
@@ -46,7 +47,13 @@ const AddServices = ({ services, selectedServices, cardList, setStep, step }) =>
                 <Heading mt={10} mb={5} fontSize={'6xl'}>Crear Cotización</Heading>
                 <SearchLook searchTerm={filter.searchTerm} setSearch={setSearch} text={"Ver cotización"} setStep={setStep} step={step} filter={filter} setFilter={setFilter} />
                 <Wrap spacing={10} justify={{ base: "center", md: "normal" }} pb={20}>
-                    {cardList(filter.filteredServices)}
+                    {filter.filteredServices.map(service => {
+                        return (
+                            <WrapItem key={service._id}>
+                                <ServiceQuote id={service._id} title={service.name} price={service.price} description={service.description} type={service.type} items={service.item.length} setSelectedServices={setSelectedServices} selectedServices={selectedServices} />
+                            </WrapItem>
+                        )
+                    })}
                 </Wrap>
             </Container >
             {selectedServices.length > 0 &&
--- pages/cotizaciones/crear.jsx
+++ pages/cotizaciones/crear.jsx
@@ -5,6 +5,7 @@ import { getCompanies } from '../../data/company';
 import AddServices from '../../components/AddServices';
 import ServiceQuote from '../../components/ServiceQuote';
 import QuoteForm from '../../components/QuoteForm';
+import QuotePreview from '../../components/QuotePreview';
 
 export async function getServerSideProps(context) {
     try {
@@ -36,11 +37,11 @@ const CrearCotizaciones = ({ data }) => {
         name: '',
         description: '',
         paymentMethod: '',
+        projectDelivery: '',
         company: '',
         payment: '',
         formalization: '',
         document: '',
-        projectDelivery: '',
     })
     const [selectedInfo, setSelectedInfo] = useState({
         company: null,
@@ -50,19 +51,9 @@ const CrearCotizaciones = ({ data }) => {
     })
     const [step, setStep] = useState(1)
 
-    const cardList = (data) => {
-        return data.map(service => {
-            return (
-                <WrapItem key={service._id}>
-                    <ServiceQuote id={service._id} title={service.name} price={service.price} description={service.description} type={service.type} items={service.item.length} setSelectedServices={setSelectedServices} selectedServices={selectedServices} />
-                </WrapItem>
-            )
-        })
-    }
-
     if (step === 1) {
         return (
-            <AddServices services={services} selectedServices={selectedServices} cardList={cardList} setStep={setStep} step={step} />
+            <AddServices services={services} selectedServices={selectedServices} setStep={setStep} step={step} setSelectedServices={setSelectedServices} />
         )
     }
     if (step === 2) {
@@ -72,24 +63,7 @@ const CrearCotizaciones = ({ data }) => {
     }
     if (step === 3) {
         return (
-            <Container maxW={"container.xl"} centerContent>
-                <Heading mt={10} mb={5} fontSize={'6xl'}>Crear Cotización</Heading>
-                <Text>Nombre de la cotización:{quote.name}</Text>
-                <Text>Descripción de la cotización: {quote.description}</Text>
-                <Text>Empresa cotizante: {quote.company}</Text>
-                <Text>Medio de pago: {quote.payment}</Text>
-                <Text>Formalización: {quote.formalization}</Text>
-                <Text>Tipo de factura: {quote.document}</Text>
-                <Text>Metodo de pago: {quote.paymentMethod}</Text>
-                <Text>Plazo de entrega: {quote.projectDelivery}</Text>
-                {selectedServices.map(service => {
-                    console.log(service)
-                    return (
-                        <Text>Servicio: {service}</Text>
-                    )
-                })}
-                <Button color={"white"} bgColor={"#7ABC63"} borderRadius={'3xl'} onClick={() => setStep(1)}>Atras</Button>
-            </Container >
+            <QuotePreview quote={quote} selectedServices={selectedServices} setStep={setStep} />
         )
     }
 }
