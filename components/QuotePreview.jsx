e50c35ffa7a08f8a852d10347f0aa8e092d9ce79
Ignacio González <ignacio.gonzalez1901@alumnos.ubiobio.cl>
2023-01-13 01:29:17 -0300

    Se agregan datos a la vista

--- components/QuotePreview.jsx
+++ components/QuotePreview.jsx
@@ -3,27 +3,29 @@ import { Heading, Text, Container, Button, HStack, Stack } from '@chakra-ui/reac
 import TagText from './TagText'
 
 const QuotePreview = ({ quote, selectedServices, setStep }) => {
+    console.log(quote)
 
     const handleSubmit = () => {
         console.log(quote)
     }
 
     return (
-        <Container maxW={"container.xl"} px={5}>
+        <Container maxW={"container.xl"} px={5} pb={10}>
             <Heading as={"h1"} mt={10} mb={5} fontSize={'6xl'} textAlign={{ base: "start", md: "center" }}>Crear Cotización</Heading>
-            <Stack py={10}>
+            <Stack pb={10}>
                 <Heading as={"h2"} fontSize={'3xl'} fontWeight={"bold"}>Vista previa de la cotización</Heading>
                 <TagText tag={"Nombre de la cotización"} data={quote.name} />
-                <TagText tag={"Empresa cotizante"} data={quote.company} />
-                <TagText tag={"Medio de pago"} data={quote.payment} />
-                <TagText tag={"Tipo de factura"} data={quote.document} />
-                <TagText tag={"Formalización"} data={quote.formalization} />
+                <TagText tag={"Empresa cotizante"} data={quote.company.name} />
+                <TagText tag={"Medio de pago"} data={quote.payment.name} />
+                <TagText tag={"Tipo de factura"} data={quote.document.name} />
+                <TagText tag={"Formalización"} data={quote.formalization.name} />
                 <TagText tag={"Metodo de pago"} data={quote.paymentMethod} />
                 <TagText tag={"Descripción de la cotización"} data={quote.description} />
                 <TagText tag={"Plazo de entrega"} data={quote.projectDelivery} />
             </Stack>
             <Stack>
                 <Heading as={"h2"} fontSize={'3xl'} fontWeight={"bold"}>Servicios cotizados:</Heading>
+                {selectedServices.length === 0 && <Text>No se han añadido servicios a la cotización</Text>}
                 {selectedServices.map((service, index) => {
                     return (
                         <TagText key={index} tag={`Servicio ${index + 1}`} data={service.title} />
