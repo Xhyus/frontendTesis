2ed013fb926f54aa7f5e047495130f6b705d90c5
Ignacio González <ignacio.gonzalez1901@alumnos.ubiobio.cl>
2023-01-16 05:18:11 -0300

    Se reemplaza tabs por cards con la informacion de los servicios cotizados

--- pages/cotizaciones/ver/[cotizacion].jsx
+++ pages/cotizaciones/ver/[cotizacion].jsx
@@ -1,10 +1,11 @@
-import { Heading, Button, Container, HStack, Stack, Tabs, TabList, Tab, TabPanel, TabPanels, Tag, TagLabel, TagLeftIcon } from '@chakra-ui/react'
+import { Heading, Button, Container, HStack, Stack, Tabs, TabList, Tab, TabPanel, TabPanels, Tag, TagLabel, TagLeftIcon, Grid, Box, Text } from '@chakra-ui/react'
 import { getQuote, deleteQuote } from '../../../data/quotes'
 import { useRouter } from 'next/router'
 import TextCopy from '../../../components/TextCopy'
 import { FaCalendarTimes, FaCalendarPlus } from 'react-icons/fa'
 import { formatDate, formatFormalization, formatTitleDetail, formatText } from '../../../utils/formatInfo'
 import Swal from 'sweetalert2'
+import TagText from '../../../components/TagText'
 
 export async function getServerSideProps(context) {
     try {
@@ -57,7 +58,7 @@ const VerCotizacion = ({ quote }) => {
                     <TagLabel>{formatDate(quote.end)}</TagLabel>
                 </Tag>
             </HStack>
-            <Heading as={"h1"} my={10} fontSize={'6xl'}>{formatTitleDetail(quote.name)}</Heading>
+            <Heading as={"h1"} my={10} fontSize={{ base: '5xl', md: '6xl' }}>{formatTitleDetail(quote.name)}</Heading>
             <HStack mb={5}>
                 <Button bgColor={"#53B6EE"} color="white" _hover={{ bgColor: "#33A7EB" }} w="full" onClick={() => router.push('/cliente/cotizacion/' + quote.url)}>Ver cotización</Button>
                 <Button bgColor={"#C1292E"} color="white" _hover={{ bgColor: "#A82428" }} w="full" onClick={() => handleDelete()}>Eliminar cotización</Button>
@@ -67,18 +68,18 @@ const VerCotizacion = ({ quote }) => {
                     <Heading size={"md"} color={"Orange"}>Datos de la empresa</Heading>
                     <TextCopy prefix={"Nombre"} data={formatText(quote.company.name)} />
                     <TextCopy prefix={"Rut"} data={quote.company.rut} />
-                    {quote.company.socialReason && <TextCopy prefix={"Razón social"} data={formatText(quote.company.socialReason)} />}
+                    {quote.company.socialReason !== null ? <TextCopy prefix={"Razón social"} data={formatText(quote.company.socialReason)} /> : null}
                     {quote.company.address && <TextCopy prefix={"Dirección"} data={formatText(quote.company.address)} />}
-                    <TextCopy prefix={"Teléfono"} data={quote.company.contact.phone} />
-                    <TextCopy prefix={"Email"} data={quote.company.contact.email} />
+                    <TextCopy prefix={"Teléfono"} data={"+569" + quote.company.phone} />
+                    <TextCopy prefix={"Email"} data={quote.company.email} />
                 </Stack>
                 <Stack justify={"center"} w={{ base: "100%", md: "50%" }} pt={{ base: 5, md: "0" }}>
                     <Heading size={"md"} color={"Orange"}>Datos de contacto</Heading>
                     <TextCopy prefix={"Nombre"} data={formatText(quote.company.contact.name)} />
                     <TextCopy prefix={"Rut"} data={quote.company.contact.rut} />
-                    <TextCopy prefix={"Teléfono"} data={quote.company.contact.phone} />
+                    <TextCopy prefix={"Teléfono"} data={"+569" + quote.company.contact.phone} />
                     <TextCopy prefix={"Email"} data={quote.company.contact.email} />
-                    {quote.company.contact.socialReason ? <TextCopy prefix={"Cargo"} data={formatText(quote.company.contact.role)} /> : <TextCopy prefix={"Rol"} data={formatText(quote.company.contact.role)} />}
+                    {quote.company.socialReason !== null ? <TagText tag={"Cargo"} data={formatText(quote.company.contact.role)} /> : <TagText tag={"Rol"} data={formatText(quote.company.contact.role)} />}
                 </Stack>
             </HStack>
             <Stack>
@@ -98,26 +99,21 @@ const VerCotizacion = ({ quote }) => {
                 <TextCopy prefix={"Descripción"} data={formatText(quote.description)} />
             </Stack>
             <Stack justify={"center"} mt={'10'} mb={'16'}>
-                <Heading as={"h2"} size={"md"} textAlign={"center"} mb={5}>Servicios cotizados</Heading>
-                <Tabs variant="enclosed">
-                    <TabList>
-                        {quote.quoteServices.map((quoteService, index) => (
-                            <Tab key={index}>{formatText(quoteService.service.name)}</Tab>
-                        ))}
-                    </TabList>
-                    <TabPanels>
-                        {quote.quoteServices.map((quoteService, index) => (
-                            <TabPanel key={index} >
-                                <Stack justify={"center"} w={{ base: "100%", md: "100%" }} >
-                                    <TextCopy prefix={"Nombre"} data={formatText(quoteService.service.name)} />
-                                    <TextCopy prefix={"Tipo de servicio"} data={formatText(quoteService.service.type)} />
-                                    <TextCopy prefix={"Descripción"} data={formatText(quoteService.service.description)} />
-                                    <TextCopy prefix={"Precio"} data={`$${quoteService.price} CLP`} />
-                                </Stack>
-                            </TabPanel>
-                        ))}
-                    </TabPanels>
-                </Tabs>
+                <Heading as={"h2"} fontSize={"3xl"} color="orange" mb={5}>Servicios cotizados</Heading>
+                <Grid templateColumns={{ base: "repeat(1, 1fr)", md: "repeat(2, 1fr)" }} gap={6}>
+                    {quote.quoteServices.map((quoteService, index) => (
+                        <Box key={index} p={5} shadow="md" borderWidth="1px" borderRadius="md" bgColor="#3E4550">
+                            <Stack justify={"center"} w={{ base: "100%", md: "100%" }} >
+                                <Heading as={"h2"} size={"md"} color={"orange"}>{formatText(quoteService.service.name)}</Heading>
+                                <TextCopy prefix={"Nombre"} data={formatText(quoteService.service.name)} />
+                                <TextCopy prefix={"Tipo de servicio"} data={formatText(quoteService.service.type)} />
+                                <Text fontWeight={"bold"}>Descripción:</Text>
+                                <Text>{formatText(quoteService.service.description)}</Text>
+                                <TextCopy prefix={"Precio"} data={`${quoteService.price} UF`} />
+                            </Stack>
+                        </Box>
+                    ))}
+                </Grid>
             </Stack>
         </Container >
     )
