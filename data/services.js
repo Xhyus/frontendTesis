70c72ba0ae0a1f9cf4b51c3f94b1a4fb071cc15f
Ignacio González <ignacio.gonzalez1901@alumnos.ubiobio.cl>
2023-03-03 02:15:05 -0300

    Fix manejo de peticion delete

--- data/services.js
+++ data/services.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const deleteServices = async (id, token) => {
-    const response = await axios.put(`${process.env.SERVIDOR}/service/delete/${id}`, { headers: { 'authorization': `bearer ${token}` } });
+    const response = await axios.put(`${process.env.SERVIDOR}/service/delete/${id}`, {}, { headers: { 'authorization': `bearer ${token}` } });
     return response;
 }
 
